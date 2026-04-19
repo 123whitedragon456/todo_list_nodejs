8a340a08f66272373d770180c1218a4b3ca04664
Toru <romaninadiana2007@gmail.com>
2026-04-19 12:10:41 +0000

    Ініціалізація проекту: додано залежності express, ejs та body-parser

---
+++ index.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let task = ["Опанувати JavaScript", "Опанувати Node.js"];
+let complete = ["Опанувати HTML та CSS", "Опанувати Git та GitHub"];
+
+app.post("/addnewtask", function (req, res) {
+    let newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/");
+});
+
+app.post("/movetocompletetask", function (req, res) {
+    let completeTask = req.body.check;
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (let i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
+app.get("/", function (req, res) {
+    res.render("index", { task: task, complete: complete });
+});
+
+app.listen(3000, function () {
+    console.log("Сервер працює на порту 3000!");
+});
